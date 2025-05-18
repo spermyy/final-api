40a9f2ecfdc4d3cc8b10bc2a03af4896cffe12ac
spermyy <manarangdaniel@gmail.com>
2025-05-18 20:48:06 +0800

    first commit

---
+++ src/test/java/com/manarang/post/FinalProjectITel3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manarang.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectITel3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
